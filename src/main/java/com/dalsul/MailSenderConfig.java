16a9e605837d7e8b4d9f6f3bb0b3dbd59546fa8b
JangYonghun <yonghun0404@gmail.com>
2023-09-13 18:00:07 +0900

    ㅎㅇㅎㅇ

--- src/main/java/com/dalsul/MailSenderConfig.java
+++ src/main/java/com/dalsul/MailSenderConfig.java
@@ -9,7 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailSenderConfig {
-	// ㅎㅇ
+	// ㅎㅇㅎㅇ
 	@Bean
     public JavaMailSender MailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
