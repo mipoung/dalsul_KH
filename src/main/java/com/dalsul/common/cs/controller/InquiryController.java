3d332547debfccf49eb42f28d265d99e18ca3da9
taehwan <thpark0903@gmail.com>
2023-09-13 15:14:41 +0900

    기존꺼 업데이트

--- src/main/java/com/dalsul/common/cs/controller/InquiryController.java
+++ src/main/java/com/dalsul/common/cs/controller/InquiryController.java
@@ -1,46 +1,141 @@
-/*
- * package com.dalsul.common.cs.controller;
- * 
- * import java.util.List;
- * 
- * import javax.servlet.http.HttpSession;
- * 
- * import org.springframework.beans.factory.annotation.Autowired; import
- * org.springframework.stereotype.Controller; import
- * org.springframework.ui.Model; import
- * org.springframework.web.bind.annotation.GetMapping; import
- * org.springframework.web.bind.annotation.ModelAttribute; import
- * org.springframework.web.bind.annotation.RequestMapping;
- * 
- * import com.dalsul.common.cs.service.InquiryService; import
- * com.dalsul.common.cs.vo.InquiryVO; import com.dalsul.common.vo.PageDTO;
- * 
- * import lombok.Setter;
- * 
- * @Controller
- * 
- * @RequestMapping("/inquiry/*") public class InquiryController {
- * 
- * @Setter(onMethod_ = @Autowired) private InquiryService inquiryService; 실험용
- * 세션부여
- * 
- * @GetMapping("setInquiryAdminSession") public String
- * setAdminSession(HttpSession session) { session.setAttribute("isAdmin", true);
- * return "redirect:/inquiry/InquiryList"; }
- * 
- * inquiry 목록, 페이지네이팅
- * 
- * @GetMapping("/InquiryList") public String InquiryList(@ModelAttribute
- * InquiryVO ivo, Model model) { List<InquiryVO> inquiryList =
- * inquiryService.inquiryList(ivo); model.addAttribute("inquiryList",
- * inquiryList);
- * 
- * int total = inquiryService.inquiryListCnt(ivo);
- * model.addAttribute("pageMaker", new PageDTO(ivo, total));
- * 
- * return "cs/inquiry/inquiryList"; }
- * 
- * 
- * 
- * }
- */
+package com.dalsul.common.cs.controller;
+
+import java.util.List;
+
+//import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.dalsul.common.cs.service.InquiryService;
+import com.dalsul.common.cs.vo.InquiryVO;
+import com.dalsul.common.vo.PageDTO;
+
+import lombok.Setter;
+
+@Controller
+
+@RequestMapping("/inquiry/*")
+public class InquiryController {
+
+	@Setter(onMethod_ = @Autowired)
+	private InquiryService inquiryService; 
+
+	/*@GetMapping("setInquiryAdminSession") // 실험용 세션부여
+	public String setAdminSession(HttpSession session) {
+		session.setAttribute("isAdmin", true);
+		return "redirect:/inquiry/inquiryList";
+	}*/
+
+
+	// inquiry 목록, 페이지네이팅
+
+	@GetMapping("/inquiryList")
+	public String InquiryList(@ModelAttribute InquiryVO ivo, Model model) {
+		List<InquiryVO> inquiryList = inquiryService.inquiryList(ivo);
+		model.addAttribute("inquiryList", inquiryList);
+
+		int total = inquiryService.inquiryListCnt(ivo);
+		model.addAttribute("pageMaker", new PageDTO(ivo, total));
+
+		return "cs/inquiry/inquiryList";
+	}
+	//글쓰기 , 작성하기 폼
+	@GetMapping("/inquiryWriteForm")
+	public String inquiryWriteForm() {
+		return "cs/inquiry/writeForm";
+	}
+	//글쓰기, 작성하기
+	@PostMapping("/inquiryInsert")
+	public String inquiryInsert(/*@SessionAttribute("userLogin") UserRegInfo rvo,*/InquiryVO ivo, Model model) throws Exception{
+		
+		int result = 0;
+		String url = "";
+		
+		result = inquiryService.inquiryInsert(ivo);
+		if(result == 1) {
+			url = "redirect:/inquiry/inquiryList";
+		} else {
+			url = "redirect:/inquiry/inquiryWriteForm";
+		}
+		return "redirect" + url;
+	}
+	
+	//글 클릭하였을때 상세보기
+	@GetMapping("/inquiryDetail")
+	public String inquiryDetail(InquiryVO ivo, Model model) {
+		InquiryVO detail = inquiryService.inquiryDetail(ivo);
+		
+		model.addAttribute("detail", detail);
+		
+		return "cs/inquiry/inquiryDetail";
+	}
+	
+	//글에대한 비밀번호
+	@ResponseBody
+	@PostMapping(value = "/pwdConfirm", produces = "text/plain; charset=UTF-8")
+	public String pwdConfirm(InquiryVO ivo) {
+		String value = "";
+		
+		int result = inquiryService.pwdConfirm(ivo);
+		if(result == 1) {
+			value = "성공!"; //추후수정
+		} else {
+			value = "실패!"; //추후수정
+		}
+		return value;
+		
+	}
+	
+	//글 수정 폼
+	@GetMapping("/inquiryUpdateForm")
+	public String inquiryUpdateForm(@ModelAttribute InquiryVO ivo, Model model) {
+		
+		InquiryVO updateData = inquiryService.inquiryUpdateForm(ivo);
+		
+		model.addAttribute("updateData", updateData);
+		
+		return "cs/inquiry/inquiryUpdateForm";
+	}
+	
+	//글 수정
+	@PostMapping("/inquiryUpdate")
+	public String inquiryupdate(@ModelAttribute InquiryVO ivo) {
+		int result = 0;
+		String url = "";
+		
+		result = inquiryService.inquiryUpdate(ivo);
+		
+		if(result == 1) {
+			url = "redirect:/inquiry/inquiryDetail?inquiry_no=" + ivo.getInquiry_no();
+		} else {
+			url = "redirect:/inquiry/inquiryUpdateForm?inquiry_no=" + ivo.getInquiry_no();
+		}
+		return "redirect:" + url;
+	}
+	@PostMapping("/inquiryDelete")
+	public String inquiryDelete(@ModelAttribute InquiryVO ivo, RedirectAttributes ras) throws Exception{
+		int result = 0;
+		String url = "";
+		
+		result = inquiryService.inquiryDelete(ivo);
+		ras.addFlashAttribute("InquiryVO", ivo);
+		
+		if(result == 1) {
+			url = "redirect:/inquiry/inquiryList";
+		} else {
+			url = "redirect:/inquiry/inquiryDetail?inquiry_no=" + ivo.getInquiry_no();
+		}
+		return "redirect:" + url;
+	}
+	
+
+}
--- src/main/java/com/dalsul/common/cs/vo/InquiryVO.java
+++ src/main/java/com/dalsul/common/cs/vo/InquiryVO.java
@@ -2,6 +2,20 @@ package com.dalsul.common.cs.vo;
 
 import com.dalsul.common.vo.CommonVO;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
 public class InquiryVO extends CommonVO{
+	private int inquiry_no = 0;			//글 번호
+	private int user_no;				//회원일련번호
+	private String inquiry_title;		//글 제목
+	private String inquiry_question;	//글 내용
+	private String inquiry_password;	//글 비밀번호
+	private String inquiry_date;		//글 작성일
+	private String inquiry_update_date;	//글 수정일
+	private String inquiry_status;		//처리상태
+	private String inquiry_whether;		//비밀글 여부
 	
 }
