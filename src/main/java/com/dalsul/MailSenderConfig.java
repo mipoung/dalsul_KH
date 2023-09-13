83646dbc79c5c30f0e9a5986379a05408b548200
오민준 <omj31@naver.com>
2023-09-13 14:01:05 +0900

    config 주석

--- src/main/java/com/dalsul/MailSenderConfig.java
+++ src/main/java/com/dalsul/MailSenderConfig.java
@@ -9,7 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailSenderConfig {
-	// yes no
+	// yes no 아
 	@Bean
     public JavaMailSender MailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
