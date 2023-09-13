d7a2c8469e1afc808d9515198f0e77ad9442aea0
JangYonghun <yonghun0404@gmail.com>
2023-09-13 18:06:09 +0900

    마스터 단독내용

--- src/main/java/com/dalsul/MailSenderConfig.java
+++ src/main/java/com/dalsul/MailSenderConfig.java
@@ -18,7 +18,7 @@ public class MailSenderConfig {
         mailSender.setPort(587); // 포트 번호 설정
         mailSender.setUsername("yonghun0404@gmail.com"); // 사용자 이름 설정
         mailSender.setPassword("qdkvqouzsyewmfoh"); // 비밀번호 설정
-        
+        // 마스터만 갖고있는 내용
         // 추가적인 SMTP 프로퍼티 설정
         Properties properties = new Properties();
         properties.setProperty("mail.smtp.timeout", "5000");
