ad392d2ce2d8883c9646e0e218ec96cc4adfb3b0
mipoung <mipoung@gmail.com>
2023-09-13 18:32:48 +0900

    test

--- src/main/java/com/dalsul/MailSenderConfig.java
+++ src/main/java/com/dalsul/MailSenderConfig.java
@@ -9,7 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailSenderConfig {
-	// 장용훈만 갖고있는 내용1
+	
 	@Bean
     public JavaMailSender MailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
@@ -18,11 +18,7 @@ public class MailSenderConfig {
         mailSender.setPort(587); // 포트 번호 설정
         mailSender.setUsername("yonghun0404@gmail.com"); // 사용자 이름 설정
         mailSender.setPassword("qdkvqouzsyewmfoh"); // 비밀번호 설정
-<<<<<<< HEAD
-        //123
-=======
-
->>>>>>> master
+     
         // 추가적인 SMTP 프로퍼티 설정
         Properties properties = new Properties();
         properties.setProperty("mail.smtp.timeout", "5000");
@@ -33,7 +29,7 @@ public class MailSenderConfig {
 
         return mailSender;
 
-     // 장용훈만 갖고있는 내용2
+     
     }
 	
 }
