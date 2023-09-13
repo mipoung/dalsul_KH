deba115932aeb8de1a8a0be818582a5a2b5293fd
mipoung <mipoung@gmail.com>
2023-09-13 17:42:05 +0900

    ㄴㅇ롬너ㅏ로ㅓㅇ

--- src/main/java/com/dalsul/MailSenderConfig.java
+++ src/main/java/com/dalsul/MailSenderConfig.java
@@ -9,11 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailSenderConfig {
-<<<<<<< HEAD
- 
-=======
-	// yes no 아
->>>>>>> master
+	
 	@Bean
     public JavaMailSender MailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
