3a857e9d835e63befa21e67b221523a37db4da3a
Oh min Joon <omj31@naver.com>
2023-09-16 14:57:09 +0900

    주석전송

--- src/main/java/com/dalsul/MailSenderConfig.java
+++ src/main/java/com/dalsul/MailSenderConfig.java
@@ -9,7 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailSenderConfig {
-
+	//주석보냅니다
 	@Bean
     public JavaMailSender MailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
