dccc5669c7da69c20b9d643c8ccce930a7ff46ef
Bokyung <w98700@naver.com>
2023-09-22 12:47:28 +0900

    ㅁㅁ

--- src/main/java/com/dalsul/user/pay/vo/PayDTO.java
+++ src/main/java/com/dalsul/user/pay/vo/PayDTO.java
@@ -13,6 +13,6 @@ public class PayDTO {
 	private int order_total_price;  //총 상품금액
 	private int order_use_coupon; //사용쿠폰
 	private String order_delivery_info; //배송지
-	private List<Integer> product_no; //product_order 상품일련번호
+	private List<String> product_no; //product_order 상품일련번호
 	private List<Integer> quantity; //상품 갯수
 }
